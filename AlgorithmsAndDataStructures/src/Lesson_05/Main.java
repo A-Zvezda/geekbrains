35a2d6cd65bcd6941f394e9a9e4c39385a21448b
Aleksandr Zvezda <zvezda_a@mail.ru>
2019-10-11 16:33:44 +0600

    Lesson 05

--- AlgorithmsAndDataStructures/src/Lesson_05/Main.java
+++ AlgorithmsAndDataStructures/src/Lesson_05/Main.java
@@ -1,4 +1,25 @@
 package Lesson_05;
 
 public class Main {
+    public static void main (String[] args) {
+        System.out.println(myPower(4,10));
+        System.out.println(myPowerQ(4,10));
+    }
+
+    public static int myPowerQ(int x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if ((n & 1) == 1) {
+            return x * myPowerQ(x, n / 2) * myPowerQ(x, n / 2);
+        }
+        return myPowerQ(x, n / 2) * myPowerQ(x, n / 2);
+    }
+
+    public static int myPower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+            return x * myPower(x, n - 1);
+        }
 }
+
