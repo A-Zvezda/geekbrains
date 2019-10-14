47c8cf9dcb833354968f12a5b29594c752eda9e9
Aleksandr Zvezda <zvezda_a@mail.ru>
2019-10-14 11:16:13 +0600

    Add solutoin.

--- AlgorithmsAndDataStructures/src/Lesson_05/Main.java
+++ AlgorithmsAndDataStructures/src/Lesson_05/Main.java
@@ -1,25 +1,82 @@
-package Lesson_05;
+package lesson5;
 
 public class Main {
-    public static void main (String[] args) {
-        System.out.println(myPower(4,10));
-        System.out.println(myPowerQ(4,10));
+    public static void main(String[] args) {
+//        System.out.println(fact(5));
+//        System.out.println(recFact(5));
+
+//        System.out.println(fibo(48));
+//        System.out.println(recFibo(10));
+
+//        System.out.println(triangleNum(5));
+//        System.out.println(recTriangleNum(5));
+
+//        System.out.println(multiply(4, 7));
+//        System.out.println(recMultiply(4, 7));
+
     }
 
-    public static int myPowerQ(int x, int n) {
-        if (n == 0) {
-            return 1;
-        } else if ((n & 1) == 1) {
-            return x * myPowerQ(x, n / 2) * myPowerQ(x, n / 2);
+    static int fact(int n) {
+        int value = 1;
+        for (int i = 1; i <= n; i++) {
+            value *= i;
         }
-        return myPowerQ(x, n / 2) * myPowerQ(x, n / 2);
+        return value;
     }
 
-    public static int myPower(int x, int n) {
-        if (n == 0) {
+    static int recFact(int n) {
+        if (n <= 1) {
             return 1;
         }
-            return x * myPower(x, n - 1);
+        return recFact(n - 1) * n;
+    }
+
+    static long fibo(int n) {
+        long a = 1;
+        long b = 1;
+        for (int i = 3; i <= n; i++) {
+            b = b + a;
+            a = b - a;
         }
+        return b;
+    }
+
+    static long recFibo(int n) {
+//        System.out.print(n +" ");
+        if (n < 3) {
+            return 1;
+        }
+        return recFibo(n - 2) + recFibo(n - 1);
+    }
+
+    static int triangleNum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    static int recTriangleNum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return recTriangleNum(n - 1) + n;
+    }
+
+    static int multiply(int a, int b) {
+        int value = 0;
+        for (int i = 0; i < b; i++) {
+            value += a;
+        }
+        return value;
+    }
+
+    static int recMultiply(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+        return recMultiply(a, b - 1) + a;
+    }
+
 }
-
